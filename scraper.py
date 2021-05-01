865e76532d4f99e6d778a001551139e96e5aa26f
starnakin <starnakin2@gmail.com>
2021-05-01 19:26:04 +0200

    datetime add, bug 2 event in same time patched, add requests

--- scraper.py
+++ scraper.py
@@ -1,31 +1,43 @@
-from bs4 import BeautifulSoup
-
 import requests
+from bs4 import BeautifulSoup 
+import datetime
 
 def get(url):
+    with requests.session() as rs:
+        rs.get(url)
+        login_data = {'referer': url}
+        respond = rs.post(url, data=login_data)
+        soup = BeautifulSoup(respond.text, 'lxml')
+
+        data= {"delivered": False}
+
+        events = {}
+
+        for date in soup.findAll("div", {"class", "infos-account"}):
+            event_date=date.findAll("strong")[0].text.split("/")
+            year = int(event_date[2])
+            month = int(event_date[1])
+            day = int(event_date[0])
+            for step in date.findAll("div", {"class", "step-suivi line-t"}):
+                try:
+                    event = step.findAll("p")[1].text.replace("\n", "")
+                    if event == "Colis livré au destinataire":
+                        data.update({"delivered": True})
+                    event_date = step.findAll("p")[0].text.replace("\n", "").split(":")
+                    hour=int(event_date[0])
+                    minute=int(event_date[1])
+                    date_data=datetime.datetime(year, month, day, hour, minute)
+                    event_data=event
+                    if events.get(date_data) == None:
+                        events.update({date_data:[event_data]})
+                    else:
+                        event_by_date = events.get(date_data)
+                        event_by_date.append(event_data)
+                        events.update({date_data:event_by_date})
+                except:
+                    pass
+
+        data.update({"events": events})
 
 
-
-    page = requests.get(url)
-
-    soup = BeautifulSoup(page.text, 'lxml')
- 
-    data= {}
-
-    date = {}
-
-    events = soup.findAll("div", {"class", "infos-account"})
-    for i in events:
-        event_date=i.findAll("strong")[0].text
-        for j in i.findAll("div", {"class", "step-suivi line-t"}):
-            try:
-                if j.findAll("p")[1].text.replace("\n", "") == "Colis livré au destinataire":
-                    data.update({"delivered": True})
-                date.update({event_date+j.findAll("p")[0].text.replace("\n", ""): j.findAll("p")[1].text.replace("\n", "")})
-            except:
-                pass
-    data.update({"events": date})
-
-    return data
-
-print(get("https://www.mondialrelay.fr/suivi-de-colis/?NumeroExpedition=97602273&CodePostal=92140"))
+        return data
